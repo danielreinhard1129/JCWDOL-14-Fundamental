62e308e85d86d1f8038f7c903c1b9732620c432b
danielreinhard1129 <119338658+danielreinhard1129@users.noreply.github.com>
2024-04-30 19:44:05 +0700

    add no 6

--- day1-exercise.js
+++ day1-exercise.js
@@ -10,9 +10,22 @@
 
 // Write a code to get difference between dates in days.
 
-const date1 = new Date("2023-11-01");
+const date1 = new Date("2023-10-05");
 const date2 = new Date("2023-10-20");
 const bedaTanggal = Math.abs(date2 - date1); // math absolute -> hasilnya akan selalu positif
 
-const bedaHari = Math.round(bedaTanggal / (1000 * 3600 * 24));
+const bedaHari = Math.round(bedaTanggal / (1000 * 3600 * 24)); // membulatkan angka
 console.log("Perbedaan hari : " + bedaHari);
+
+// ======================================================================
+
+// Write a code to convert days to years, months and days.
+// Example : 400 days → 1 year, 1 month, 5 days
+// 1 year : 365 days, 1 month : 30 days
+
+const hari = 400;
+const tahun = Math.floor(hari / 365);
+const bulan = Math.floor((hari % 365) / 30);
+const tanggal = Math.floor((hari % 365) % 30);
+
+console.log(tahun + " tahun " + bulan + " bulan " + tanggal + " hari");
